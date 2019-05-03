3ffd8671cfda63943ed8051826663ddc2a7eabd7
mt-user <ecines@gmail.com>
2019-05-03 11:32:49 +0200

    Add keyboard
    Adds styles and keyboard with options. You must input the correct code.

--- chronobomb/src/app/clock/clock.component.ts
+++ chronobomb/src/app/clock/clock.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import {FormControl, FormGroup} from '@angular/forms';
+
 const DEFAULT_MINUTES = 9;
 const DEFAULT_SECONDS = 59;
 
@@ -10,16 +12,20 @@ const DEFAULT_SECONDS = 59;
 export class ClockComponent implements OnInit {
   constructor() {
     this.resetTimer();
-    setInterval(() => this.actions(), 1000);
+    this.chrono = setInterval(() => this.actions(), 1000);
   }
   ngOnInit(): void {
     this.resetTimer();
   }
   private minutos: string;
   private segundos: string;
+  private gifVisibility: boolean;
+  private chrono;
 
   private min: number;
   private sec: number;
+
+  private screen = { code: "", isCode: false };
 
   private actions = () => {
     const pipe = (...fns) => fns.map(f => f());
@@ -33,8 +39,7 @@ export class ClockComponent implements OnInit {
     if (--this.sec < 0) {
       this.sec = DEFAULT_SECONDS;
       if (--this.min < 0) {
-        this.min = DEFAULT_MINUTES;
-        this.sec = DEFAULT_SECONDS;
+        this.explode();
       }
     }
   };
@@ -50,10 +55,37 @@ export class ClockComponent implements OnInit {
     }
   };
 
-  private resetTimer(): void {
+  private resetTimer = () => {
     this.min = DEFAULT_MINUTES;
     this.sec = DEFAULT_SECONDS;
-  }
+  };
 
-  public getTime = () => `${this.minutos} : ${this.segundos}`;
+  private explode = () => {
+      clearInterval(this.chrono);
+      this.min = 0;
+      this.sec = 0;
+      this.gifVisibility = true;
+  };
+
+  public getTime = () => `${this.minutos}:${this.segundos}`;
+
+  public teclado = (entrada: string) => {
+    switch(entrada){
+      case "del":
+        this.screen.code = this.screen.code.slice(0,this.screen.code.length - 1);
+        break;
+      case "ok":
+        if (this.screen.code == "41253"){
+          clearInterval(this.chrono);
+        } else {
+          this.explode();
+        }
+        break;
+      default:
+        if (this.screen.code.length < 5) {
+          this.screen.code += entrada;
+        }
+        break;
+    }
+  };
 }
